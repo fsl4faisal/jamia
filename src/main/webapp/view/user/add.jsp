a765036e64a6a76770ba36b15548d05fa83abb3a
Md Faisal <fsl4faisal@gmail.com>
2016-03-08 15:25:34 +0530

    add user bootstrapped

--- src/main/webapp/view/user/add.jsp
+++ src/main/webapp/view/user/add.jsp
@@ -1,30 +1,71 @@
+<!DOCTYPE html>
+
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script
+	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
 <title>Add User</title>
 </head>
 <body>
-	<h1>Add User</h1>
-	<form action="user" method="post">
-		<input type="hidden" name="add">
-		<ul>
-			<li>Name:<input type="text" name="name"></li>
-			<li>Role:<select name="role">
-			<option value="0">(none)</option>
-					<c:forEach var="role" items="${roles}">
-						<option value="${role.value}">${role.name}</option>
-					</c:forEach>			
-				</select>	
-			</li>
-			<li>Email Address:<input type="text" name="email_address"></li>
-		</ul>
-		<input type="submit" value="Submit" name="add">
-	</form>
-	<a href="adminHome">Back to Home</a>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="col-md-4">
+					<h2>Add User</h2>
+					<form action="user" method="post" role="form">
+
+
+						<div class="form-group ">
+							<label for="user">User Name:</label> <input type="text"
+								class="form-control" name="name" id="name"
+								placeholder="Enter User Name">
+						</div>
+
+
+						<div class="form-group">
+							<label for="email">Email Address:</label> <input type="email"
+								name="email_address" class="form-control" id="email"
+								placeholder="Enter email Address">
+						</div>
+
+						<div class="form-group">
+							<label for="role">Role:</label> <select name="role"
+								class="form-control" id="role" placeholder="Enter Role">
+								<option value="0">(none)</option>
+								<c:forEach var="role" items="${roles}">
+									<option value="${role.value}">${role.name}</option>
+								</c:forEach>
+							</select>
+						</div>
+
+						<button type="submit" value="Submit" name="add"
+							class="btn btn-primary active">Submit</button>
+						<a href="adminHome" class="btn btn-info" role="button">Back</a>
+
+					</form>
+
+
+				</div>
+				<div class="col-md-4">
+					<div class="col-md-4"></div>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
--- target/tomcat/work/localEngine/localhost/JamiaExamPrototype/org/apache/jsp/view/user/add_jsp.java
+++ target/tomcat/work/localEngine/localhost/JamiaExamPrototype/org/apache/jsp/view/user/add_jsp.java
@@ -54,34 +54,75 @@ public final class add_jsp extends org.apache.jasper.runtime.HttpJspBase
       out = pageContext.getOut();
       _jspx_out = out;
 
+      out.write("<!DOCTYPE html>\r\n");
       out.write("\r\n");
       out.write("\r\n");
       out.write("\r\n");
-      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\r\n");
+      out.write("\r\n");
       out.write("<html>\r\n");
       out.write("<head>\r\n");
       out.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">\r\n");
+      out.write("\r\n");
+      out.write("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n");
+      out.write("\r\n");
+      out.write("<link rel=\"stylesheet\"\r\n");
+      out.write("\thref=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\">\r\n");
+      out.write("<script\r\n");
+      out.write("\tsrc=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js\"></script>\r\n");
+      out.write("<script\r\n");
+      out.write("\tsrc=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\"></script>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
       out.write("<title>Add User</title>\r\n");
       out.write("</head>\r\n");
       out.write("<body>\r\n");
-      out.write("\t<h1>Add User</h1>\r\n");
-      out.write("\t<form action=\"user\" method=\"post\">\r\n");
-      out.write("\t\t<input type=\"hidden\" name=\"add\">\r\n");
-      out.write("\t\t<ul>\r\n");
-      out.write("\t\t\t<li>Name:<input type=\"text\" name=\"name\"></li>\r\n");
-      out.write("\t\t\t<li>Role:<select name=\"role\">\r\n");
-      out.write("\t\t\t<option value=\"0\">(none)</option>\r\n");
-      out.write("\t\t\t\t\t");
+      out.write("\r\n");
+      out.write("\t<div class=\"container\">\r\n");
+      out.write("\t\t<div class=\"row\">\r\n");
+      out.write("\t\t\t<div class=\"col-md-12\">\r\n");
+      out.write("\t\t\t\t<div class=\"col-md-4\">\r\n");
+      out.write("\t\t\t\t\t<h2>Add User</h2>\r\n");
+      out.write("\t\t\t\t\t<form action=\"user\" method=\"post\" role=\"form\">\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\t\t\t\t\t\t<div class=\"form-group \">\r\n");
+      out.write("\t\t\t\t\t\t\t<label for=\"user\">User Name:</label> <input type=\"text\"\r\n");
+      out.write("\t\t\t\t\t\t\t\tclass=\"form-control\" name=\"name\" id=\"name\"\r\n");
+      out.write("\t\t\t\t\t\t\t\tplaceholder=\"Enter User Name\">\r\n");
+      out.write("\t\t\t\t\t\t</div>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\t\t\t\t\t\t<div class=\"form-group\">\r\n");
+      out.write("\t\t\t\t\t\t\t<label for=\"email\">Email Address:</label> <input type=\"email\"\r\n");
+      out.write("\t\t\t\t\t\t\t\tname=\"email_address\" class=\"form-control\" id=\"email\"\r\n");
+      out.write("\t\t\t\t\t\t\t\tplaceholder=\"Enter email Address\">\r\n");
+      out.write("\t\t\t\t\t\t</div>\r\n");
+      out.write("\r\n");
+      out.write("\t\t\t\t\t\t<div class=\"form-group\">\r\n");
+      out.write("\t\t\t\t\t\t\t<label for=\"role\">Role:</label> <select name=\"role\"\r\n");
+      out.write("\t\t\t\t\t\t\t\tclass=\"form-control\" id=\"role\" placeholder=\"Enter Role\">\r\n");
+      out.write("\t\t\t\t\t\t\t\t<option value=\"0\">(none)</option>\r\n");
+      out.write("\t\t\t\t\t\t\t\t");
       if (_jspx_meth_c_005fforEach_005f0(_jspx_page_context))
         return;
-      out.write("\t\t\t\r\n");
-      out.write("\t\t\t\t</select>\t\r\n");
-      out.write("\t\t\t</li>\r\n");
-      out.write("\t\t\t<li>Email Address:<input type=\"text\" name=\"email_address\"></li>\r\n");
-      out.write("\t\t</ul>\r\n");
-      out.write("\t\t<input type=\"submit\" value=\"Submit\" name=\"add\">\r\n");
-      out.write("\t</form>\r\n");
-      out.write("\t<a href=\"adminHome\">Back to Home</a>\r\n");
+      out.write("\r\n");
+      out.write("\t\t\t\t\t\t\t</select>\r\n");
+      out.write("\t\t\t\t\t\t</div>\r\n");
+      out.write("\r\n");
+      out.write("\t\t\t\t\t\t<button type=\"submit\" value=\"Submit\" name=\"add\"\r\n");
+      out.write("\t\t\t\t\t\t\tclass=\"btn btn-primary active\">Submit</button>\r\n");
+      out.write("\t\t\t\t\t\t<a href=\"adminHome\" class=\"btn btn-info\" role=\"button\">Back</a>\r\n");
+      out.write("\r\n");
+      out.write("\t\t\t\t\t</form>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\t\t\t\t</div>\r\n");
+      out.write("\t\t\t\t<div class=\"col-md-4\">\r\n");
+      out.write("\t\t\t\t\t<div class=\"col-md-4\"></div>\r\n");
+      out.write("\t\t\t\t</div>\r\n");
+      out.write("\t\t\t</div>\r\n");
+      out.write("\t\t</div>\r\n");
+      out.write("\t</div>\r\n");
       out.write("</body>\r\n");
       out.write("</html>");
     } catch (Throwable t) {
@@ -104,23 +145,23 @@ public final class add_jsp extends org.apache.jasper.runtime.HttpJspBase
     org.apache.taglibs.standard.tag.rt.core.ForEachTag _jspx_th_c_005fforEach_005f0 = (org.apache.taglibs.standard.tag.rt.core.ForEachTag) _005fjspx_005ftagPool_005fc_005fforEach_0026_005fvar_005fitems.get(org.apache.taglibs.standard.tag.rt.core.ForEachTag.class);
     _jspx_th_c_005fforEach_005f0.setPageContext(_jspx_page_context);
     _jspx_th_c_005fforEach_005f0.setParent(null);
-    // /view/user/add.jsp(19,5) name = var type = java.lang.String reqTime = false required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    // /view/user/add.jsp(50,8) name = var type = java.lang.String reqTime = false required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
     _jspx_th_c_005fforEach_005f0.setVar("role");
-    // /view/user/add.jsp(19,5) name = items type = javax.el.ValueExpression reqTime = true required = false fragment = false deferredValue = true expectedTypeName = java.lang.Object deferredMethod = false methodSignature = null
-    _jspx_th_c_005fforEach_005f0.setItems(new org.apache.jasper.el.JspValueExpression("/view/user/add.jsp(19,5) '${roles}'",_el_expressionfactory.createValueExpression(_jspx_page_context.getELContext(),"${roles}",java.lang.Object.class)).getValue(_jspx_page_context.getELContext()));
+    // /view/user/add.jsp(50,8) name = items type = javax.el.ValueExpression reqTime = true required = false fragment = false deferredValue = true expectedTypeName = java.lang.Object deferredMethod = false methodSignature = null
+    _jspx_th_c_005fforEach_005f0.setItems(new org.apache.jasper.el.JspValueExpression("/view/user/add.jsp(50,8) '${roles}'",_el_expressionfactory.createValueExpression(_jspx_page_context.getELContext(),"${roles}",java.lang.Object.class)).getValue(_jspx_page_context.getELContext()));
     int[] _jspx_push_body_count_c_005fforEach_005f0 = new int[] { 0 };
     try {
       int _jspx_eval_c_005fforEach_005f0 = _jspx_th_c_005fforEach_005f0.doStartTag();
       if (_jspx_eval_c_005fforEach_005f0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
         do {
           out.write("\r\n");
-          out.write("\t\t\t\t\t\t<option value=\"");
+          out.write("\t\t\t\t\t\t\t\t\t<option value=\"");
           out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.proprietaryEvaluate("${role.value}", java.lang.String.class, (PageContext)_jspx_page_context, null, false));
           out.write('"');
           out.write('>');
           out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.proprietaryEvaluate("${role.name}", java.lang.String.class, (PageContext)_jspx_page_context, null, false));
           out.write("</option>\r\n");
-          out.write("\t\t\t\t\t");
+          out.write("\t\t\t\t\t\t\t\t");
           int evalDoAfterBody = _jspx_th_c_005fforEach_005f0.doAfterBody();
           if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
             break;
